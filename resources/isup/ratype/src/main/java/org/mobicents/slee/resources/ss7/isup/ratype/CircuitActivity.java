9cab6c7a7b7fd7a3122dcfd1cce6a029ddfbaa79
George Yanev <ygeorge@applicata.bg>
2015-05-15 10:32:54 +0300

    Add CircuitActivity type - for call session and for maintenance

--- resources/isup/ratype/src/main/java/org/mobicents/slee/resources/ss7/isup/ratype/CircuitActivity.java
+++ resources/isup/ratype/src/main/java/org/mobicents/slee/resources/ss7/isup/ratype/CircuitActivity.java
@@ -6,14 +6,24 @@ import javax.slee.SLEEException;
 import javax.slee.resource.ActivityAlreadyExistsException;
 import javax.slee.resource.StartActivityException;
 
+import org.mobicents.protocols.ss7.isup.message.BlockingMessage;
+import org.mobicents.protocols.ss7.isup.message.CircuitGroupBlockingMessage;
+import org.mobicents.protocols.ss7.isup.message.CircuitGroupResetMessage;
+import org.mobicents.protocols.ss7.isup.message.CircuitGroupUnblockingMessage;
 import org.mobicents.protocols.ss7.isup.message.ISUPMessage;
 import org.mobicents.protocols.ss7.isup.ParameterException;
+import org.mobicents.protocols.ss7.isup.message.ResetCircuitMessage;
+import org.mobicents.protocols.ss7.isup.message.UnblockingMessage;
 
 public class CircuitActivity implements Serializable {
-	
+
+	public static final int CALL_SESSION = 0;
+	public static final int MAINTENANCE = 1;
+
 	private RAISUPProvider isupProvider;
 	private int cic;
 	private int dpc;
+	private int type = CALL_SESSION;
 	
 	/**
 	 * Create client transaction activity
@@ -31,8 +41,27 @@ public class CircuitActivity implements Serializable {
 		this.cic=message.getCircuitIdentificationCode().getCIC();
 		this.dpc=dpc;
 		this.isupProvider=isupProvider;
+		this.type = getType(message);
 	}
-	
+
+	public static int getType(ISUPMessage message) {
+		switch (message.getMessageType().getCode()) {
+			case BlockingMessage.MESSAGE_CODE:
+			case UnblockingMessage.MESSAGE_CODE:
+			case CircuitGroupBlockingMessage.MESSAGE_CODE:
+			case CircuitGroupUnblockingMessage.MESSAGE_CODE:
+			case CircuitGroupResetMessage.MESSAGE_CODE:
+			case ResetCircuitMessage.MESSAGE_CODE:
+				return CircuitActivity.MAINTENANCE;
+			default:
+				return CircuitActivity.CALL_SESSION;
+		}
+	}
+
+	public int getType() {
+		return type;
+	}
+
 	public void sendMessage(ISUPMessage msg) throws ParameterException, IOException
 	{
 	    if(msg == null ){
@@ -62,13 +91,20 @@ public class CircuitActivity implements Serializable {
 	
 	public long getTransactionKey()
 	{
-		return CircuitActivity.generateTransactionKey(cic, dpc);
+		return CircuitActivity.generateTransactionKey(cic, dpc, type);
 	}
 	
-	public static long generateTransactionKey(int cic,int dpc)
+	public static long generateTransactionKey(int cic,int dpc, int code)
 	{
 		long currValue=dpc;
 		currValue=(currValue<<14) + (long)cic;
+		currValue= (currValue<<8) + code;
 		return currValue;
+	}
+
+	@Override
+	public String toString() {
+		return "CircuitActivity(cic = " + cic + "; dpc = " + dpc + "; type = " +
+				(type == CALL_SESSION ? "CALL":"MAINTENANCE") + ")";
 	}
 }
